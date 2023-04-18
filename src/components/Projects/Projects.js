339a00ccb7cdfb86da7a9195cf435139e4a34e41
mazeenkd <jm_cadi@esi.dz>
2023-04-18 17:28:36 +0100

    3rd version

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,8 +29,8 @@ function Projects() {
               description="a web application for ESI that would allow users to
               users to follow the progress of a case by each structure in coordination with coordination with the others in real time, which would also allow the creation of a new database for each financial year, notify users of changes in files and thus
               notifying users of file changes and thus filtering multiple files according to different search criteria."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              ghLink="https://github.com/chamsou-Br/Projet-"
+              demoLink=""
             />
           </Col>
 
@@ -40,8 +40,8 @@ function Projects() {
               isBlog={false}
               title="Gestin"
               description="G-ESTIN is a web-based application that allows the Head of Department, through a friendly, intuitive and easy-to-manage graphical interface, to manage ESTIN departments by easily generating timetables, exam schedules, managing Pedagogical Councils (CPs) which conclude with printable minutes and deliberations. ."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
-              demoLink="https://blogs.soumya-jit.tech/"
+              ghLink="https://github.com/mazeenkd/gestin/tree/mazene"
+              demoLink=""
             />
           </Col>
 
@@ -51,8 +51,8 @@ function Projects() {
               isBlog={false}
               title="enigma"
               description="The aim is to produce a simplified electronic version of the enigma machine (a single reflector)."
-              ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              ghLink="https://github.com/mazeenkd/enigma"
+              demoLink=""              
             />
           </Col>
 
@@ -66,8 +66,8 @@ function Projects() {
               Ability to automatically order missing ingredients for a recipe using Alexa.
               Provide a family space to add a shopping list.
               Make statistics by calories consumption."
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
+              ghLink="https://github.com/chamsou-Br/IWD_2022_TEAM_S400"
+              demoLink=""
             />
           </Col>
 
@@ -77,8 +77,8 @@ function Projects() {
               isBlog={false}
               title="BrEYEn - devfest21"
               description="BrEYEne is a mobile app under the theme of Accessibility and inclusion which gives more accessibility to the disabled people."
-              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
-              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+              ghLink="https://github.com/chechna9/Devfest-Hackathon2021"
+              // demoLink="" <--------Please include a demo link here
             />
           </Col>
 
@@ -88,8 +88,8 @@ function Projects() {
               isBlog={false}
               title="Sonatrach ROP Prediction - Haick23 datathon"
               description="training a machine learning model that can accurately predict the Rate of Penetration (ROP) in drilling operations using real-world data from the Algerian energy company, Sonatrach."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+              ghLink=""
+              // demoLink=""      <--------Please include a demo link here 
             />
           </Col>
         </Row>
